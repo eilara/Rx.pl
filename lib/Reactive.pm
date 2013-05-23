6db67bf005edf7c63cfd3d5ffac7eea31415e44e
Shlomi Fish <shlomif@shlomifish.org>
2013-05-23 22:55:57 +0300

    Add more POD Coverage.

--- lib/Reactive.pm
+++ lib/Reactive.pm
@@ -12,6 +12,18 @@ our $VERSION = '0.0.1';
 use base 'Exporter';
 our @EXPORT=qw(Observable);
 
+=head1 METHODS
+
+=head2 $class->loop()
+
+Start the main loop.
+
+=head2 $class->unloop()
+
+Stop the main loop.
+
+=cut
+
 sub loop   { EV::loop }
 sub unloop { EV::unloop }
 
