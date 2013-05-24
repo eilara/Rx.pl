d3d7bba269d3d7e1305c4c3d87a14ed6cbfb18e6
Shlomi Fish <shlomif@shlomifish.org>
2013-05-24 14:09:00 +0300

    Add the run-pod-coverage.pl script.
    One can use it to run the pod coverage and it alone as quickly as
    allowed by Dist-Zilla. Useful for debugging.

---
+++ scripts/run-pod-coverage.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use autodie;
+
+use IO::All;
+
+
+my $ini = 'dist.ini';
+
+my $orig_text = io($ini)->all;
+io($ini)->print($orig_text =~ s/^;\s*(\Q[PodCoverageTests]\E\s*)$/$1/mrs);
+system("dzil", "build");
+io($ini)->print($orig_text);
+
+my $dir = 'Reactive-0.0.1';
+
+system("export RELEASE_TESTING=1; cd $dir && perl Build.PL && ./Build && prove t/*pod-cover*.t");
