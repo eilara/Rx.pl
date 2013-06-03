f3892755b32a9216c9215e069c3f195daa2c3db1
Shlomi Fish <shlomif@shlomifish.org>
2013-06-03 21:26:32 +0300

    Pod cover for lib/Reactive/Observer/Wrapper.pm.

--- lib/Reactive/Observer/Wrapper.pm
+++ lib/Reactive/Observer/Wrapper.pm
@@ -12,6 +12,58 @@ use aliased 'Reactive::Observer::Empty' => 'Empty';
 has disposable_parent => (is => 'ro', required => 1, weak_ref => 1);
 has wrap              => (is => 'ro', required => 1);
 
+=encoding utf8
+
+=head1 NAME
+
+Reactive::Observer::Wrapper - base class for wrapping an observer.
+
+=head1 SYNOPSIS
+
+    use Moose;
+
+    has count => (is => 'ro', required => 1);
+    has taken => (is => 'rw', default  => 0);
+
+    extends 'Reactive::Observer::Wrapper';
+
+    sub on_next {
+        my ($self, $value) = @_;
+        $self->wrap->on_next($value);
+        my $taken = $self->taken + 1;
+        $self->taken($taken);
+        $self->on_complete if $taken >= $self->count;
+    }
+
+    1;
+
+=head1 METHODS
+
+=head2 $self->wrap()
+
+A read only slot for the observer getting wrapped.
+
+=head2 $self->disposable_parent()
+
+A disposable wrapping subscription to wrapped observable of wrapped observer
+also the subscription of this observer and thus must be weak, for outside
+control of the subscription. B<TODO:> surely there is a better way to explain
+that.
+
+=head2 $self->on_next()
+
+Wraps wrap()’s on_next().
+
+=head2 $self->on_complete().
+
+Wraps wrap()’s on_complete().
+
+=head2 $self->on_error()
+
+Wraps wrap()’s on_error().
+
+=cut
+
 sub on_next {
     my ($self, $value) = @_;
     local $_ = $value;
@@ -32,17 +84,42 @@ sub on_error {
 }
 
 # has my subscription vanished, could be before we complete or unwrap
+
+=head2 is_disposing()
+
+Whether the current parent has been disposed.
+
+=cut
+
 sub is_disposing { !defined(shift->{disposable_parent}) }
+
+=head2 $self->wrap_with_parent($child)
+
+Make disposable_parent() wrap $child as well.
+
+=cut
 
 sub wrap_with_parent {
     my ($self, $child) = @_;
     $self->disposable_parent->wrap($child);
 }
 
+=head2 $self->unwrap_parent(@args)
+
+Call disposable_parent()’s unwrap with @args .
+
+=cut
+
 sub unwrap_parent {
     my ($self, @args) = @_;
     $self->disposable_parent->unwrap(@args);
 }
+
+=head2 $self->unwrap()
+
+Unwrap $self->wrap().
+
+=cut
 
 sub unwrap {
     my $self = shift;
