155ba62e65bcdaf1c79fb1ef2dc824fc1e748201
Shlomi Fish <shlomif@shlomifish.org>
2013-07-06 16:04:11 +0300

    More POD::Coverage.
    This time to Reactive::Observer::Empty.

--- lib/Reactive/Observer/Empty.pm
+++ lib/Reactive/Observer/Empty.pm
@@ -12,3 +12,29 @@ sub on_error    {}
 
 1;
 
+=encoding utf8
+
+=head1 NAME
+
+Reactive::Observer::Empty - an empty observer.
+
+=head1 SYNOPSIS
+
+This is an empty observer that does nothing.
+
+=head1 METHODS
+
+=head2 on_next
+
+Does nothing.
+
+=head2 on_complete
+
+Does nothing.
+
+=head2 on_error
+
+Does nothing.
+
+=cut
+
